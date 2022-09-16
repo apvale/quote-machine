eacaf63d6cccb7c842a7c49b9996e213b8ede3a3
Ana Paula Vale <apvalefin@gmail.com>
2022-09-16 14:28:10 +0100

    chore: try to use codeowners (#3)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const quotes = [
   'The Best Way To Get Started Is To Quit Talking And Begin Doing.',
   'The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.',
   'Don’t Let Yesterday Take Up Too Much Of Today.',
+  'One more quote',
 ]
 
 function newQuote() {
