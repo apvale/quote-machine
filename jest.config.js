4e0c88ec3be9f6574f8a9bb83d935d9b0653654d
Ana Paula Vale <apvalefin@gmail.com>
2022-10-06 10:58:09 +0100

    update gitignore

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,12 @@ module.exports = async () => {
   return {
     verbose: true,
     coverageReporters: ["clover", "json", "lcov", ["text", {skipFull: true}]],
+    collectCoverage: true,
+    collectCoverageFrom: ["./src/**"],
+    coverageThreshold: {
+      global: {
+        lines: 90,
+      },
+    },
   }
 }
